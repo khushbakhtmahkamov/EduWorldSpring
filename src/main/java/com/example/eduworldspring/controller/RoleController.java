cbff597a653175cfc137954e13e371cd2f04bcf7
Muazzam2006 <mmuazzam677@gmail.com>
2025-07-05 16:23:55 +0500

    create RoleService, RoleController, RoleMapper
    Feature/edu world 15

--- src/main/java/com/example/eduworldspring/controller/RoleController.java
+++ src/main/java/com/example/eduworldspring/controller/RoleController.java
@@ -1,13 +1,10 @@
 package com.example.eduworldspring.controller;
 
-import com.example.eduworldspring.mapper.RoleMapper;
-import com.example.eduworldspring.model.Role;
 import com.example.eduworldspring.service.RoleService;
 import com.example.eduworldspring.dto.role.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -17,34 +14,24 @@ public class RoleController {
     @Autowired
     private RoleService roleService;
 
-    @Autowired
-    private RoleMapper roleMapper;
-
     @GetMapping
     public List<RoleDto> getAllRoles() {
-        List<Role> roles = roleService.getAllRoles();
-        List<RoleDto> res = new ArrayList<>();
-
-        for (Role role : roles) {
-            res.add(roleMapper.toDto(role));
-        }
-
-        return res;
+        return roleService.getAllRoles();
     }
 
     @PostMapping
-    public Role createRole(@RequestBody RoleCreateDto dto) {
-        return roleService.createRole(roleMapper.toModel(dto));
+    public RoleDto createRole(@RequestBody RoleCreateDto dto) {
+        return roleService.createRole(dto);
     }
 
     @GetMapping("/{id}")
     public RoleDto getRoleById(@PathVariable Long id) {
-        return roleMapper.toDto(roleService.getById(id));
+        return roleService.getById(id);
     }
 
     @PutMapping("/{id}")
-    public Role updateRole(@PathVariable Long id, @RequestBody RoleUpdateDto dto) {
-        return roleService.updateRole(id, roleMapper.toModel(id, dto));
+    public RoleDto updateRole(@PathVariable Long id, @RequestBody RoleUpdateDto dto) {
+        return roleService.updateRole(id, dto);
     }
 
     @DeleteMapping("/{id}")
