dc7bdd1ac42bd87693aadaaca5f61e579d5f2141
Joeknow03 <jahongirakiljanov@gmail.com>
2025-07-06 19:10:30 +0500

    EduWorld-37 update

---
+++ src/main/java/com/example/eduworldspring/dto/task/TaskCreateDto.java
@@ -0,0 +1,18 @@
+package com.example.eduworldspring.dto.task;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TaskCreateDto {
+    private String question;
+    private String start_date;
+    private String end_date;
+    private boolean isActive;
+    private int level;
+    private Long typeId;
+    private Long lessonId;
+}
