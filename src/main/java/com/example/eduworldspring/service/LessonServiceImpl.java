4b0c1462a6cf8d220111babcd484bdf52b8419e0
IbrohimKom <ibrohimappinventor@gmail.com>
2025-07-07 15:47:35 +0300

    Update lesson code and adding error processing

--- src/main/java/com/example/eduworldspring/service/LessonServiceImpl.java
+++ src/main/java/com/example/eduworldspring/service/LessonServiceImpl.java
@@ -1,30 +1,44 @@
 package com.example.eduworldspring.service;
 
+import com.example.eduworldspring.exceptions.BusinessExceptionCode;
+import com.example.eduworldspring.exceptions.BusinessRuntimeException;
+import com.example.eduworldspring.mapper.LessonMapper;
 import com.example.eduworldspring.model.Lesson;
 import com.example.eduworldspring.dto.lesson.LessonCreateUpdateDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
-public class LessonServiceImpl implements LessonService{
+@RequiredArgsConstructor
+public class LessonServiceImpl implements LessonService {
     private ArrayList<Lesson> lessons = new ArrayList<>();
+    private final LessonMapper lessonMapper;
+
     @Override
     public Lesson createLesson(LessonCreateUpdateDto lessonCreateUpdateDto) {
-        return null;
+        if (lessonCreateUpdateDto == null) {
+            throw new BusinessRuntimeException(BusinessExceptionCode.BAD_REQUEST, "Lesson cannot be null");
+
+        }
+        Lesson lesson = lessonMapper.toLesson(lessonCreateUpdateDto, ThreadLocalRandom.current().nextLong(1, 100));
+        lessons.add(lesson);
+        return lesson;
     }
 
     @Override
     public Lesson getLesson(Long id) {
         if (id == null) {
-            return null;
+            throw new BusinessRuntimeException(BusinessExceptionCode.BAD_REQUEST, "Lesson id cannot be null");
         }
         for (Lesson lesson : lessons) {
             if (lesson.getLesson_id().equals(id)) {
                 return lesson;
             }
         }
-        return null;
+        throw new BusinessRuntimeException(BusinessExceptionCode.NOT_FOUND, "Lesson id is not found");
     }
 
     @Override
@@ -33,17 +47,28 @@ public class LessonServiceImpl implements LessonService{
     }
 
     @Override
-    public Boolean updateLesson(LessonCreateUpdateDto lessonCreateUpdateDto, Long id) {
-        if (id == null || lessonCreateUpdateDto == null) {
-            return false;
+    public Boolean updateLesson(Long id, LessonCreateUpdateDto lessonCreateUpdateDto) {
+        if (id == null) {
+            throw new BusinessRuntimeException(BusinessExceptionCode.BAD_REQUEST, "id cannot be null");
         }
-        return false;
+        if (lessonCreateUpdateDto == null) {
+            throw new BusinessRuntimeException(BusinessExceptionCode.BAD_REQUEST, "LessonCreateUpdateDto cannot be null");
+        }
+
+        Lesson updatedLesson = lessonMapper.toLesson(lessonCreateUpdateDto, id);
+        for (int i = 0; i < lessons.size(); i++) {
+            if (lessons.get(i).getLesson_id().equals(id)) {
+                lessons.set(i, updatedLesson);
+                return true;
+            }
+        }
+        throw new BusinessRuntimeException(BusinessExceptionCode.NOT_FOUND, "Lesson id is not found");
     }
 
     @Override
     public Boolean deleteLesson(Long id) {
         if (id == null) {
-            return false;
+            throw new BusinessRuntimeException(BusinessExceptionCode.BAD_REQUEST, "id cannot be null");
         }
         for (Lesson lesson : lessons) {
             if (lesson.getLesson_id().equals(id)) {
@@ -51,6 +76,6 @@ public class LessonServiceImpl implements LessonService{
                 return true;
             }
         }
-        return false;
+        throw new BusinessRuntimeException(BusinessExceptionCode.NOT_FOUND, "Lesson id is not found");
     }
 }
