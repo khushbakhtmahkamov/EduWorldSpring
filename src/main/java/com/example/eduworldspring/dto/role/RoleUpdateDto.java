33f339aaf032ea69f4d44e6d032d7a8c3f28d021
Muazzam2006 <mmuazzam677@gmail.com>
2025-07-07 17:41:52 +0500

    create RoleService, RoleController, RoleMapper
    Feature/edu world 15

--- src/main/java/com/example/eduworldspring/dto/role/RoleUpdateDto.java
+++ src/main/java/com/example/eduworldspring/dto/role/RoleUpdateDto.java
@@ -1,5 +1,10 @@
 package com.example.eduworldspring.dto.role;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class RoleUpdateDto {
     private String name;
     private String description;
@@ -13,27 +18,4 @@ public class RoleUpdateDto {
         this.active = active;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public boolean isActive() {
-        return active;
-    }
-
-    public void setActive(boolean active) {
-        this.active = active;
-    }
 }
