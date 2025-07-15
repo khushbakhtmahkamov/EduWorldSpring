12a467f244ebc646bdd309f851a62a114d965981
Joeknow03 <jahongirakiljanov@gmail.com>
2025-07-15 21:02:02 +0500

    update Task file

--- src/main/java/com/example/eduworldspring/model/Task.java
+++ src/main/java/com/example/eduworldspring/model/Task.java
@@ -1,14 +1,26 @@
 package com.example.eduworldspring.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@Entity
+@Table(name = "tasks")
 public class Task {
     private Long taskId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "lesson_id")
+    private Lesson lesson;
+
     private String question;
     private String start_date;
     private String end_date;
@@ -19,4 +31,7 @@ public class Task {
 
     public Task () {}
 
+    public String getCorrectAnswer() {
+        return "";
+    }
 }
