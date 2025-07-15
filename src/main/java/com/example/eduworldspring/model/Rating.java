3ccefc2d5eee4000fbbd62910d280c029b0d8ce3
Joeknow03 <jahongirakiljanov@gmail.com>
2025-07-15 20:36:47 +0500

    update

--- src/main/java/com/example/eduworldspring/model/Rating.java
+++ src/main/java/com/example/eduworldspring/model/Rating.java
@@ -1,48 +1,48 @@
-package com.example.eduworldspring.model;
-
-public class Rating{
-
-private Long id;
-private User user;
-private Double grade;
-private Progress progress;
-
-    public Rating(Long id, User user, Double grade, Progress progress) {
-        this.id = id;
-        this.user = user;
-        this.grade = grade;
-        this.progress = progress;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public Double getGrade() {
-        return grade;
-    }
-
-    public void setGrade(Double grade) {
-        this.grade = grade;
-    }
-
-    public Progress getProgress() {
-        return progress;
-    }
-
-    public void setProgress(Progress progress) {
-        this.progress = progress;
-    }
-}
+//package com.example.eduworldspring.model;
+//
+//public class Rating{
+//
+//private Long id;
+//private User user;
+//private Double grade;
+//private Progress progress;
+//
+//    public Rating(Long id, User user, Double grade, Progress progress) {
+//        this.id = id;
+//        this.user = user;
+//        this.grade = grade;
+//        this.progress = progress;
+//    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public User getUser() {
+//        return user;
+//    }
+//
+//    public void setUser(User user) {
+//        this.user = user;
+//    }
+//
+//    public Double getGrade() {
+//        return grade;
+//    }
+//
+//    public void setGrade(Double grade) {
+//        this.grade = grade;
+//    }
+//
+//    public Progress getProgress() {
+//        return progress;
+//    }
+//
+//    public void setProgress(Progress progress) {
+//        this.progress = progress;
+//    }
+//}
